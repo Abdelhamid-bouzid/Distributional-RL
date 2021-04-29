8a405aadf185f6d069ea96bc7a74e32006f0b2e2
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-04-29 12:24:54 -0400

    Update DeepQnetwork.py

--- Distributional Reinforcement Learning with Quantile Regression/DeepQnetwork.py
+++ Distributional Reinforcement Learning with Quantile Regression/DeepQnetwork.py
@@ -1,27 +1,58 @@
-import torch
-from torch import nn
+import os
+import torch as T
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from .q_network import QNetwork
+import numpy as np
 
 class DeepQnetwork(nn.Module):
-    def __init__(self, lr, state_size, action_size, N, hiddens=[64], seed=0):
+    def __init__(self, lr, input_dim, n_actions, N):
         super(DeepQnetwork, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.hiddens = hiddens
-        self.action_size = action_size
-        self.N = N
-        self.q_network = QNetwork(state_size, hiddens[-1], hiddens[:-1], seed = seed)
-        self.last_layer = nn.Linear(hiddens[-1], action_size * N)
         
-        self.optimizer = optim.RMSprop(self.parameters(), lr=lr)
-        self.device    = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.check_dir = check_dir
+        self.path      = os.path.join(check_dir,model_name)
+        self.N         = N
+        
+        self.conv1     = nn.Conv2d(input_dim[0], 32, 8, stride=4)
+        self.conv2     = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv3     = nn.Conv2d(64, 64, 3, stride=1)
+        
+        fc_input_dims  = self.calculate_conv_output_dims(input_dim)
+        
+        self.fc1       = nn.Linear(fc_input_dims, 512)
+        self.fc2       = nn.Linear(512, n_actions*self.N)
+        
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss      = nn.MSELoss()
+        
+        self.device    = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
-
-    def forward(self, x):
-        x = F.relu(self.q_network(x))
-        x = self.last_layer(x)
+    
+    def forward(self, state):
+        x = F.relu(self.conv1(state))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        
+        x = x.view(x.size()[0], -1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         x = x.reshape(-1, self.action_size, self.N)
-
+ 
         return x
+    
+    def calculate_conv_output_dims(self, input_dim):
+        state = T.zeros(1, *input_dim)
+        dims = self.conv1(state)
+        dims = self.conv2(dims)
+        dims = self.conv3(dims)
+        return int(np.prod(dims.size()))
+    
+    def save_model(self):
+        print('################## saving model ########################')
+        T.save(self.state_dict(), self.path)
+    
+    def load_model(self):
+        print('################## loading model ########################')
+        self.load_state_dict(T.load(self.path))
 
